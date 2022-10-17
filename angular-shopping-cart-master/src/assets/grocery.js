d9cbbb11a76c068eb842ce28218e534aab90b816
VasaviNagaraju02 <233666@ust.com>
2022-10-17 17:50:53 +0530

    Initial commit

---
+++ angular-shopping-cart-master/src/assets/grocery.js
@@ -0,0 +1,183 @@
+export const inventory = [
+    
+    {
+        "name": "Milk",
+        "price": 10,
+        "category": ["Dairy"]
+    },
+    {
+        "name": "Boost",
+        "price": 20,
+        "category": ["Dairy"]
+    },
+    {
+        "name": "Horlicks",
+        "price": 20,
+        "category": ["Dairy"]
+    },
+    {
+        "name": "Badam Milk",
+        "price": 30,
+        "category": ["Dairy"]
+    },
+    {
+        "name": "Coffe",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Black Coffe",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Latte",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Espresso",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Cappuccino",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Caffe Mocha",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Cold Brew",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Irish Coffee",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Americano",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Chai",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Black Chai",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Green Chai",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Ginger Chai",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Masala Chai",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Lemon Chai",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Mint Chai",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name":"Herbal Chai",
+        "price": 30,
+        "Category":["Dairy"]
+    },
+    {
+        "name": "Egg Puffs",
+        "price": 20,
+        "category": ["Dairy"]
+    },
+    {
+        "name": "Veg Puffs",
+        "price": 15,
+        "category": ["Dairy"]
+    },
+    {
+        "name": "Veg Spring Roll",
+        "price": 45,
+        "category": ["Fruit"]
+    },
+    {
+        "name": "Chicken Spring Roll",
+        "price": 45,
+        "category": ["Fruit"]
+    },
+    {
+        "name": "Veg Sandwich",
+        "price": 80,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "chicken Sandwich",
+        "price": 90,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "Paneer Sandwich",
+        "price": 90,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "Samosa",
+        "price": 25,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "Cookies",
+        "price": 30,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "Paneer Pakoda",
+        "price": 60,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "Aloo Tikki",
+        "price": 80,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "Medu Vadu",
+        "price": 25,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "chai Biscuits",
+        "price": 90,
+        "category": ["Dairy","Snack"]
+    },
+    {
+        "name": "Bread",
+        "price": 30,
+        "category": ["Dairy"]
+    },
+    {
+        "name": "Chip",
+        "price": 15,
+        "category": ["Snack"]
+    }
+];
